43a5b5b6138b47875cd0b2a9bb19bf1ea30c138a
Amirametwally <amira.metwally56@gmail.com>
2023-09-19 22:01:38 +0300

    adding some function

---
+++ stringTwo.c
@@ -0,0 +1,78 @@
+#include "shell.h"
+
+/**
+ * _startWith - function that locates a substring
+ * @haystack: string to search through
+ * @needle: string to search for
+ *
+ * Return: null
+ */
+char *_startWith(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+/**
+ * _putchar - write charachter to stdout
+ * @c: charachter
+ *
+ * Return: 1 or -1
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _puts - print string
+ * @str: the string to be printed
+ *
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _strcpy - copies a string
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
