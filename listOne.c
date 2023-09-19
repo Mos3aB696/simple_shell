a8737550f9dff87019fb0799e47c7a7952ecbbe8
Mos3aB696 <mos3ab696@gmail.com>
2023-09-19 12:15:17 +0300

    add listOne file (*add_node | *add_node_end | print_list_str | delete_node_at_index)

---
+++ listOne.c
@@ -0,0 +1,141 @@
+#include "shell.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: head of node
+ * @str: the string to add as head
+ * @num: node index
+ *
+ * Return: data
+ */
+
+list_t *add_node(list_t **head, const char *str, int num)
+{
+	list_t *data;
+
+	if (!head)
+		return (NULL);
+
+	data = malloc(sizeof(list_t));
+	_memoryset((void *)data, 0, sizeof(list_t));
+	data->num = num;
+	if (str)
+	{
+		data->data = _strdup(str);
+		if (!data->data)
+		{
+			free(data);
+			return (NULL);
+		}
+	}
+	data->next = *head;
+	*head = data;
+	return (data);
+}
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: the pointer to struct
+ * @str: the string to dup
+ * @num: node index;
+ *
+ * Return: data
+ */
+
+list_t *add_node_end(list_t **head, const char *str, int num)
+{
+	list_t *data, *current;
+
+	if (!head)
+		return (NULL);
+	current = *head;
+	data = malloc(sizeof(list_t));
+	if (!data)
+		return (NULL);
+	_memoryset((void *)data, 0, sizeof(list_t));
+	data->num = num;
+	if (str)
+	{
+		data->data = _strdup(str);
+		if (!data->data)
+		{
+			free(data);
+			return (NULL);
+		}
+	}
+	if (current)
+	{
+		while (current->next)
+			current = current->next;
+		current->next = data;
+	}
+	else
+		*head = data;
+	return (data);
+}
+
+/**
+ * print_list_str - prints only str elements
+ * @head: the head node
+ *
+ * Return: i
+ */
+
+size_t print_list_str(const list_t *head)
+{
+	size_t i;
+
+	while (head)
+	{
+		if (head->data)
+			_puts(head->data);
+		else
+			_puts("(nil)");
+		_puts("\n");
+		head = head->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * delete_node_at_index - function that deletes the node
+ * at index of a list_t linked list.
+ * @head: the head of struct
+ * @index: the index
+ *
+ * Return: (1) on succeeded
+ *					(-1) on failed
+ */
+
+int delete_node_at_index(list_t **head, unsigned int index)
+{
+	list_t *cur, *prev;
+	unsigned int i = 0;
+
+	if (!*head)
+		return (-1);
+
+	cur = *head;
+	prev = NULL;
+
+	if (!index)
+	{
+		*head = (*head)->next;
+		free(cur);
+		return (1);
+	}
+	for (i = 0; i < index; i++)
+	{
+		if (!cur)
+			return (-1);
+		prev = cur;
+		cur = cur->next;
+	}
+	if (!cur)
+		return (-1);
+
+	prev->next = cur->next;
+	free(cur);
+	return (1);
+}
